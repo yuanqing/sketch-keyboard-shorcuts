b22bdfed8fcf307c08176fda0152d467ce358ff7
yuanqing <desk@yuanqing.sg>
2019-05-11 16:23:38 +0800

    Add aliases

--- src/common/keyboard.js
+++ src/common/keyboard.js
@@ -2,9 +2,12 @@ export const ESCAPE_KEY = '\u001b'
 
 export const KEYBOARD_KEY_MAP = {
   cmd: '@',
+  command: '@',
+  control: '^',
   ctrl: '^',
   down: '↓',
   left: '←',
+  opt: '~',
   option: '~',
   right: '→',
   shift: '\\$',
